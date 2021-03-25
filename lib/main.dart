1c239fdd3f31dee8e8905275cd1a21b7537cc489
Vardaan Sharma <gokuldh@gmail.com>
2021-03-25 20:23:23 +0530

    Day 4 Part 2
    Notes App

--- lib/main.dart
+++ lib/main.dart
@@ -5,134 +5,52 @@ void main(){
   runApp(FlutterBootcamp());
 }
 
-class FlutterBootcamp extends StatelessWidget{
+class FlutterBootcamp extends StatefulWidget{
+  @override
+  _FlutterBootcamp createState() => _FlutterBootcamp();
+
+}
+
+class _FlutterBootcamp extends State<FlutterBootcamp>{
+
+  List<String> notes = [
+    "Maintain your Car!",
+    "Do the Project",
+    "Finish it by Tonight!",
+    "Get Groceries",
+    "My name is Vardaan"
+  ];
+
+  Widget cardTemplate(singlenote){
+    return Card(
+      shadowColor: Colors.deepOrange,
+      margin: EdgeInsets.all(5),
+      child: Padding(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          children: [
+            Text(singlenote),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
    return MaterialApp(
      home: Scaffold(
        appBar: AppBar(
+         title: Text("Notes App"),
          centerTitle: true,
-         backgroundColor: Colors.deepOrange,
-         title: Text(Strings.titletext),
+         backgroundColor: Colors.green,
        ),
        body: Column(
-         children: [
-           Row(
-             children: [
-               Expanded(
-                 flex: 1,
-                 child: Container(
-                   padding: EdgeInsets.all(20),
-                   color: Colors.deepOrange,
-                   child: Text("A"),
-                 ),
-               ),
-               Expanded(
-                 flex: 4,
-                 child: Container(
-                   padding: EdgeInsets.all(20),
-                   color: Colors.blue,
-                   child: Text("B"),
-                 ),
-               ),
-               Expanded(
-                 flex: 2,
-                 child: Container(
-                   padding: EdgeInsets.all(20),
-                   color: Colors.green,
-                   child: Text("C"),
-                 ),
-               ),
-               Expanded(
-                 flex: 4,
-                 child: Container(
-                   padding: EdgeInsets.all(20),
-                   color: Colors.yellow,
-                   child: Text("D"),
-                 ),
-               ),
-             ],
-           ),
-           Divider(
-           ),
-           Row(
-             children: [
-               Expanded(
-                 flex: 4,
-                 child: Container(
-                   padding: EdgeInsets.all(20),
-                   color: Colors.deepOrange,
-                   child: Text("A"),
-                 ),
-               ),
-               Expanded(
-                 flex: 3,
-                 child: Container(
-                   padding: EdgeInsets.all(20),
-                   color: Colors.blue,
-                   child: Text("B"),
-                 ),
-               ),
-               Expanded(
-                 flex: 3,
-                 child: Container(
-                   padding: EdgeInsets.all(20),
-                   color: Colors.green,
-                   child: Text("C"),
-                 ),
-               ),
-               Expanded(
-                 flex: 1,
-                 child: Container(
-                   padding: EdgeInsets.all(20),
-                   color: Colors.yellow,
-                   child: Text("D"),
-                 ),
-               ),
-             ],
-           ),
-           SizedBox(
-             height: 20,
-           ),
-           Row(
-             children: [
-               Expanded(
-                 flex: 2,
-                 child: CircleAvatar(
-                   backgroundImage: AssetImage('assets/sky.jpg'),
-                   radius: 100,
-                 ),
-               ),
-               Expanded(
-                 flex: 1,
-                 child: Container(
-                   padding: EdgeInsets.all(20),
-                   color: Colors.blue,
-                   child: Text("B"),
-                 ),
-               ),
-               Expanded(
-                 flex: 2,
-                 child: Container(
-                   padding: EdgeInsets.all(20),
-                   color: Colors.green,
-                   child: Text("C"),
-                 ),
-               ),
-               Expanded(
-                 flex: 2,
-                 child: Container(
-                   padding: EdgeInsets.all(20),
-                   color: Colors.yellow,
-                   child: Text("D"),
-                 ),
-               ),
-             ],
-           ),
-         ],
+         crossAxisAlignment: CrossAxisAlignment.stretch,
+         children: notes.map((singlenote) => cardTemplate(singlenote)).toList(),
        ),
      ),
    );
   }
+
 }
